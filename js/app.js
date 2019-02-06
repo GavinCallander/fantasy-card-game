fa705dc6a91ec7a3bca1bf62a5cec4bc6cad39b9
Gavin Callander <gavincallander@Gavins-MacBook-Pro.local>
2019-02-06 13:24:14 -0800

    player draw JS added

--- js/app.js
+++ js/app.js
@@ -1,24 +1,5 @@
 console.log("Up and running!");
 // player starts with three cards in their hand
-
-var playerDeck = [
-    {
-        name: "thing 1",
-        atk: 1,
-        hp: 2
-    },
-    {
-        name: "thing 2",
-        atk: 2,
-        hp: 3
-    },
-    {
-        name: "thing 3",
-        atk: 3,
-        hp: 4
-    }
-];
-
 var compDeck = [
     {
         name: "stuff 1",
@@ -34,6 +15,33 @@ var compDeck = [
         name: "stuff 3",
         atk: 3,
         hp: 3
+    },
+    {
+        name: "stuff 4",
+        atk: 4,
+        hp: 4
+    }
+];
+var playerDeck = [
+    {
+        name: "thing 1",
+        atk: 1,
+        hp: 2
+    },
+    {
+        name: "thing 2",
+        atk: 2,
+        hp: 3
+    },
+    {
+        name: "thing 3",
+        atk: 3,
+        hp: 4
+    },
+    {
+        name: "thing 4",
+        atk: 4,
+        hp: 5
     }
 ];
 // Declare DOM variables
@@ -45,6 +53,9 @@ var playerDisc = [];
 var compDisc = [];
 var cHand = document.querySelectorAll(".chandcard");
 var pHand = document.querySelectorAll(".phandcard");
+var pDeck = document.querySelector("#playerdeck");
+var cField = document.querySelectorAll("cfieldcard");
+var pField = document.querySelectorAll("pfieldcard");
 
 
 
@@ -56,11 +67,11 @@ document.addEventListener("DOMContentLoaded", function() {
         compHand.push(compDeck.shift());
         playerHand.push(playerDeck.shift());
     };
-    showCompCards();
-    showPlayerCards();
+    showCompHandCards();
+    showPlayerHandCards();
 });
 // show back of all cards in comp hand  *** Replace with image
-function showCompCards() {
+function showCompHandCards() {
     compHand.forEach(function(card, i) {
         cHand[i].children[0].textContent = card.name;
         cHand[i].children[1].textContent = card.atk;
@@ -68,16 +79,25 @@ function showCompCards() {
     });
 };
 // show values of all cards in player hand *** Style once completed
-function showPlayerCards() {
+function showPlayerHandCards() {
     playerHand.forEach(function(card, i) {
         pHand[i].children[0].textContent = card.name;
         pHand[i].children[1].textContent = card.atk;
         pHand[i].children[2].textContent = card.hp;
     });
 };
-
-
-
-
-
-
+// player draws new card
+function playerDrawCard() {
+    pDeck.addEventListener("click", function() {
+        for (let i = 0; i < 1; i++) {
+            playerHand.push(playerDeck.shift());
+        }; showPlayerHandCards();
+    });
+};
+playerDrawCard();
+// click event on div to push attached var to field
+    // click on card in pHand to select
+    // click on space in playerField to push value from playerHand
+function playerFieldCard() {
+    
+}
