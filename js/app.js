ff215386825398fc9b337a609cebac1a2b4ccff9
Gavin Callander <gavincallander@Gavins-MacBook-Pro.local>
2019-02-09 14:16:41 -0800

    basic comp AI and turn system almost completed

--- js/app.js
+++ js/app.js
@@ -1,74 +1,160 @@
-console.log("Up and running!");
-// player starts with three cards in their hand
+console.log("running");
+// player deck arrays
 var playerDeck = [
     {
-        name: "stuff 1",
+        name: "one",
+        image: "",
         atk: 1,
         hp: 1,
     },
     {
-        name: "stuff 2",
+        name: "two",
+        image: "",
         atk: 1,
         hp: 1,
     },
     {
-        name: "stuff 3",
-        atk: 3,
-        hp: 3
-    },
-    {
-        name: "stuff 4",
-        atk: 4,
-        hp: 4
-    },
-];
-var compDeck = [
-    {
-        name: "thing 1",
+        name: "three",
+        image: "",
         atk: 1,
         hp: 2
     },
     {
-        name: "thing 2",
+        name: "four",
+        image: "",
+        atk: 10,
+        hp: 10
+    },
+    {
+        name: "five",
+        image: "",
         atk: 2,
         hp: 3
     },
     {
-        name: "thing 3",
+        name: "six",
+        image: "",
+        atk: 3,
+        hp: 2
+    },
+    {
+        name: "seven",
+        image: "",
         atk: 3,
         hp: 4
     },
     {
-        name: "thing 4",
+        name: "eight",
+        image: "",
+        atk: 4,
+        hp: 3
+    },
+    {
+        name: "nine",
+        image: "",
         atk: 4,
         hp: 5
+    },
+    {
+        name: "ten",
+        image: "",
+        atk: 5,
+        hp: 4
     }
-];
-// Declare DOM variables
+]
+var compDeck = [
+    {
+        name: "eleven",
+        image: "",
+        atk: 1,
+        hp: 1
+    },
+    {
+        name: "twelve",
+        image: "",
+        atk: 1,
+        hp: 1
+    },
+    {
+        name: "thirteen",
+        image: "",
+        atk: 1,
+        hp: 2
+    },
+    {
+        name: "fourteen",
+        image: "",
+        atk: 2,
+        hp: 1
+    },
+    {
+        name: "fifteen",
+        image: "",
+        atk: 2,
+        hp: 3
+    },
+    {
+        name: "sixteen",
+        image: "",
+        atk: 3,
+        hp: 2
+    },
+    {
+        name: "seventeen",
+        image: "",
+        atk: 3,
+        hp: 4
+    },
+    {
+        name: "eighteen",
+        image: "",
+        atk: 4,
+        hp: 3
+    },
+    {
+        name: "nineteen",
+        image: "",
+        atk: 4,
+        hp: 5
+    },
+    {
+        name: "twenty",
+        image: "",
+        atk: 5,
+        hp: 4
+    }
+]
+// DOM variables
 var playerHand = [];
 var compHand = [];
 var playerField = [];
 var compField = [];
 var playerDisc = [];
 var compDisc = [];
+var playerLp = 10;
+var compLp = 10;
 var battleField = [];
-var cHand = document.querySelectorAll(".chandcard");
 var pHand = document.querySelectorAll(".phandcard");
-var cField = document.querySelectorAll(".cfieldcard");
+var pHandName = document.querySelector(".phname");
+var pHandAtk = document.querySelector(".phatk");
+var pHandHp = document.querySelector(".phhp");
+var cHand = document.querySelectorAll(".chandcard");
 var pField = document.querySelectorAll(".pfieldcard");
+var cField = document.querySelectorAll(".cfieldcard");
+var pHero = document.querySelector("#playerhero");
+var cHero = document.querySelector("#comphero");
 var pDeck = document.querySelector("#playerdeck");
-//turn variables
+// turn variables
 var pTurn = false;
-var pDraw = false;
-var pAtk = false;
-var pBattle = false;
 var cTurn = false;
+var pDraw = false;
 var cDraw = false;
+var pAtk = false;
 var cAtk = false;
+var pBattle = false;
 var cBattle = false;
-// picture variables
-
-// initialize game and place cards in player and comp hands from deck
+var turnOne = true;
+// DOMContentLoaded and game initialization
 document.addEventListener("DOMContentLoaded", function() {
     for (let i = 0; i < 3; i++) {
         compHand.push(compDeck.shift());
@@ -78,8 +164,10 @@ document.addEventListener("DOMContentLoaded", function() {
     turn();
     showCompHandCards();
     showPlayerHandCards();
+    showPlayerLp();
+    showCompLp();
 });
-// function to set starting player
+// randomize starting player ** revise - repetition, could refactor to make DRY
 function startingPlayer() {
     let num = Math.floor(Math.random() * 10);
     let num1 = num%2;
@@ -89,7 +177,6 @@ function startingPlayer() {
         cTurn = true;
     };console.log(pTurn);
 };
-// select whose turn it is
 function turn() {
     if (pTurn === true) {
         playerTurn();
@@ -97,80 +184,28 @@ function turn() {
         compTurn();
     };
 };
-// player turn logic
+// ***PLAYER MOVE LOGIC***
+// draw card
 function playerTurn() {
-    if (pDraw === false) {
+    if (pDraw === false && playerDeck.length > 0) {
         pDeck.addEventListener("click", playerDrawCard);
     };
-    if (pAtk === false) {
-        pHand.forEach(function(card) {
-        card.addEventListener("click", playerFieldCard);
-    });
-    };
-    if (pBattle === false) {
-        pField.forEach(function(card) {
-        card.addEventListener("click", declareBattle);
-    });
-    };
-}; 
-// computer turn logic  ** revise logic so can only happen when false
-function compTurn() {
-    if (cDraw === false) {
-        setInterval(compDrawCard, 2000);
-        cDraw = true;
-    };
-    if (cAtk === false) {
-        setInterval(compFieldCard, 2000);
-        cAtk = true;
-    }
 };
-
-
-// show back of all cards in comp hand  *** Replace with image
-function showCompHandCards() {
-    compHand.forEach(function(card, i) {
-        cHand[i].children[0].textContent = card.name;
-        cHand[i].children[1].textContent = card.atk;
-        cHand[i].children[2].textContent = card.hp;
-        cHand[i].setAttribute("data-name", card.name);
-    });
-};
-// show values of all cards in player hand *** Style once completed
-function showPlayerHandCards() {
-    playerHand.forEach(function(card, i) {
-        pHand[i].children[0].textContent = card.name;
-        pHand[i].children[1].textContent = card.atk;
-        pHand[i].children[2].textContent = card.hp;
-        pHand[i].setAttribute("data-name", card.name);
-    });
-};
-// player draws new card
 function playerDrawCard() {
     if (playerDeck.length > 0) {
         for (let i = 0; i < 1; i++) {
             playerHand.push(playerDeck.shift());
-        }; 
-        showPlayerHandCards();
-        pDraw = true;
-        pDeck.removeEventListener("click", playerDrawCard);
-    } else {
-        console.log("No more cards!");
-    }
-};
-// computer draws new card
-function compDrawCard() {
-    if (compDeck.length > 0) {
-        for (let i = 0; i < 1; i++) {
-            compHand.push(compDeck.shift());
         };
-        setTimeout(showCompHandCards(), 2000);
-        cDraw = true;
+        showPlayerHandCards();
+        pDeck.removeEventListener("click", playerDrawCard);
+        pHand.forEach(function(card) {
+            card.addEventListener("click", playerFieldCard);
+        });
     } else {
         console.log("No more cards!");
     };
 };
-// ******* Prevent more than one card being played *******
-// player plays card from hand - card is removed from playerHand and moved to playerField
+// field card
 function playerFieldCard() {
     for (let i = 0; i < playerHand.length; i++) {
         if (this.dataset.name === playerHand[i].name) {
@@ -179,77 +214,197 @@ function playerFieldCard() {
             pHand[i].children[1].textContent = "";
             pHand[i].children[2].textContent = "";
         }; 
-        showPlayerFieldCards(); 
+        showPlayerFieldCards();
+        pHand.forEach(function(card) {
+            card.removeEventListener("click", playerFieldCard);
+        });
+        if (turnOne === true) {
+            pHero.addEventListener("click", endTurn);
+        }
+        pField.forEach(function(card) {
+            card.addEventListener("click", playerBattle);
+        })
     };
 };
-// comp plays card from hand - card is removed from compHand and moved to compField
+// atk (once per card)************************
+    // if cards on comp side, battle
+// choose the card player wants to attack with
+function playerBattle() {
+    for (let i = 0; i < playerField.length; i++) {
+        if (this.dataset.name === playerField[i].name) {
+            battleField.push(playerField[i]);
+            console.log(battleField);
+            cField.forEach(function(card) {
+                card.addEventListener("click", playerSetUpBattle);
+            });
+        };
+        for (let x = 0; x < compField.length; i++) {
+            battleField.push(compField[x]);
+        };
+        let a = battleField[1].hp - battleField[0].atk;
+        let b = battleField[0].hp - battleField[1].atk;
+        if (a < 1) {
+
+        }
+    };  
+};
+// choose the computer card to attack
+function playerSetUpBattle() {
+        console.log("CLICK!");
+        for (let i = 0; i < compField.length; i++) {
+            if (this.dataset.name === compField[i].name) {
+                battleField.push(compField[i]);
+                console.log(battleField);
+            };
+        };
+    };
+// battle result
+function battleResult() {
+    
+} 
+    
+    // if nothing, direct hit
+
+// if no win, end turn
+function playerEndTurn() {
+    turnOne = false;
+    compTurn();
+};
+
+
+// ***COMP MOVE LOGIC***
+// comp AI
+function compTurn() {
+    if (cDraw === false && compDeck.length > 0) {
+        setTimeout(compDrawCard, 2000);
+    };
+};
+// draw card
+function compDrawCard() {
+    for (let i = 0; i < 1; i++) {
+        compHand.push(compDeck.shift());
+    };
+    showCompHandCards();
+    setTimeout(compFieldCard, 2000);
+};
+// field card
 function compFieldCard() {
     let i = Math.floor(Math.random() * compHand.length);
+        console.log(i);
         compField.push(compHand.splice(i, 1)[0]);
-        cHand[compHand.length].children[0].textContent = "";
-        cHand[compHand.length].children[1].textContent = "";
-        cHand[compHand.length].children[2].textContent = "";
+        cHand[i].children[0].textContent = "";
+        cHand[i].children[1].textContent = "";
+        cHand[i].children[2].textContent = "";
     showCompFieldCards();
+    if (turnOne === false) {
+        setTimeout(compBattle, 2000);
+    } else {
+        setTimeout(endTurn, 2000);
+    };
 };
-// show cards that have entered playerField in pField
+// atk (once per card)
+    // if cards on player field, battle
+function compBattle() {
+    let i = Math.floor(Math.random() * compField.length);
+        battleField.push(compField[i]);
+    let x = Math.floor(Math.random() * playerField.length);
+        battleField.push(playerField[x]);
+    let a = (battleField[1].hp - battleField[0].atk);
+    let b = (battleField[0].hp - battleField[1].atk);
+        console.log(b);
+    if (a < 1) {
+        playerDisc.push(playerField.splice(i, 1));
+        pField[x].children[0].textContent = "";
+        pField[x].children[1].textContent = "";
+        pField[x].children[2].textContent = "";
+    }
+    if (b < 1) {
+        playerDisc.push(playerField.splice(i, 1));
+        cField[i].children[0].textContent = "";
+        cField[i].children[1].textContent = "";
+        cField[i].children[2].textContent = "";
+    }
+    if (a < 0) {
+        playerLp = playerLp + a;
+        showPlayerLp();
+    };
+    if (b < 0) {
+        compLp = compLp + b;
+        showCompLp();
+    };
+};
+// battle result
+    // if not, direct hit
+
+// if no win, end turn
+
+// *** END TURN AND GAME LOGIC ***
+function endTurn() {
+    console.log("click!");
+    if (pTurn === true) {
+        pTurn = false;
+        cTurn = true;
+        turnOne = false;
+        compTurn();
+    } else {
+        cTurn = false;
+        pTurn = true;
+        turnOne = false;
+        playerTurn();
+    };
+};
+
+function endGame() {
+    if (pLp < 1) {
+        console.log("Computer Wins!");
+    } else if (cLp < 1) {
+        console.log("Player Wins!");
+    };
+};
+
+// ***PLAYER DISPLAY LOGIC***
+function showPlayerLp() {
+    pHero.textContent = playerLp;
+};
+// display cards in player hand
+function showPlayerHandCards() {
+    playerHand.forEach(function(card, i) {
+        pHand[i].children[0].textContent = card.name;
+        pHand[i].children[1].textContent = card.atk;
+        pHand[i].children[2].textContent = card.hp;
+        pHand[i].setAttribute("data-name", card.name);
+    });
+};
+// display cards in player field
 function showPlayerFieldCards() {
     playerField.forEach(function(card, i) {
         pField[i].children[0].textContent = card.name;
-        pField[i].children[1].textContent = card.image;
+        pField[i].children[1].src = "";
         pField[i].children[2].textContent = card.atk;
         pField[i].children[3].textContent = card.hp;
         pField[i].setAttribute("data-name", card.name);
     });
 };
+// ***COMP DISPLAY LOGIC***
+function showCompLp() {
+    cHero.textContent = compLp;
+};
+// display cards in comp hnd
+function showCompHandCards() {
+    compHand.forEach(function(card, i) {
+        cHand[i].children[0].textContent = card.name;
+        cHand[i].children[1].textContent = card.atk;
+        cHand[i].children[2].textContent = card.hp;
+        cHand[i].setAttribute("data-name", card.name);
+    });
+};
+// display cards in comp field
 function showCompFieldCards() {
     compField.forEach(function(card, i) {
         cField[i].children[0].textContent = card.name;
         cField[i].children[1].textContent = "";
         cField[i].children[2].textContent = card.atk;
         cField[i].children[3].textContent = card.hp;
-        pField[i].setAttribute("data-name", card.name);
+        cField[i].setAttribute("data-name", card.name);
     });
-};
-
-// click card in field to attack opponent
-    // click event on player card *
-    // click event on enemy card *
-        // enemy card hp - player card atk
-        // if answer is negative add to enemy hp
-
-function DeclareBattle() {
-    for (let i = 0; i < playerField.length; i++) {
-        if (this.dataset.name === playerField[i].name) {
-            battleField.push(playerField[i]);
-            console.log(battleField);
-            cField.forEach(function(card) {
-                card.addEventListener("click", setUpBattle);
-            });
-        };
-    };
-};
-//     battleField.push();
-//     cField.forEach(function(card) {
-//         card.addEventListener("click", setUpBattle);
-//     });
-// };
-
-function setUpBattle() {
-    console.log("CLICK!");
-    for (let i = 0; i < compField.length; i++) {
-        if (this.dataset.name === compField[i].name) {
-            battleField.push(compField[i]);
-            console.log(battleField);
-        }
-    }
-}
-
-function endTurn() {
-    if (pTurn === true) {
-        pTurn = false;
-        cTurn = true;
-    } else {
-        pTurn = true;
-        cTurn = false;
-    };
 };
